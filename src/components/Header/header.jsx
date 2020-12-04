4c9364dd6982d176369d1036fa93007f4762a20a
Arvind M <arvind@kraftshala.com>
2020-12-04 19:54:08 +0530

    navigation scroll into view implemeneted

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -6,9 +6,36 @@ const Header = () => {
     <div className="header_container">
       <div className="container">
         <nav className="nav_links">
-          <div className="nav_link">About</div>
-          <div className="nav_link">Projects</div>
-          <div className="nav_link">Skills</div>
+          <div
+            className="nav_link"
+            onClick={() =>
+              document
+                .querySelector('.about_container')
+                .scrollIntoView({ behavior: 'smooth' })
+            }
+          >
+            About
+          </div>
+          <div
+            className="nav_link"
+            onClick={() =>
+              document
+                .querySelector('.projects_container')
+                .scrollIntoView({ behavior: 'smooth' })
+            }
+          >
+            Projects
+          </div>
+          <div
+            className="nav_link"
+            onClick={() =>
+              document
+                .querySelector('.skills_container')
+                .scrollIntoView({ behavior: 'smooth' })
+            }
+          >
+            Skills
+          </div>
           <div className="contact_cta">
             <Link to="/contact" className="contact-btn">
               Contact
