178270f2b7866579d775b8020804eab076a8288f
Arvind M <arvind@kraftshala.com>
2020-12-04 21:53:47 +0530

    react router changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home/home';
 import Contact from './pages/Contact/contact';
 
 function App() {
   return (
-    <HashRouter>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path="/contact" component={Contact}></Route>
-          <Route component={Home}></Route>
-        </Switch>
-      </BrowserRouter>
-    </HashRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/contact" component={Contact}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
