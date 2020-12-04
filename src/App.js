861cc26f5bc34d8deb21014460108110bc0e636d
Arvind M <arvind@kraftshala.com>
2020-12-04 21:34:47 +0530

    hash router added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom';
 import Home from './pages/Home/home';
 import Contact from './pages/Contact/contact';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/contact" component={Contact}></Route>
-      </Switch>
-    </BrowserRouter>
+    <HashRouter basename="/">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/contact" component={Contact}></Route>
+        </Switch>
+      </BrowserRouter>
+    </HashRouter>
   );
 }
 
